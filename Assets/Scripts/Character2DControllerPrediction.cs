c843bf06747778add8d04e5b72520e6dfeac55c2
KongFourThousand <natthakrit.kornpaphawe@gmail.com>
2022-08-25 12:08:43 +0700

    Player movement

---
+++ Assets/Scripts/Character2DControllerPrediction.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FishNet;
+using UnityEngine;
+using FishNet.Example.Prediction.CharacterControllers;
+using FishNet.Object;
+using FishNet.Object.Prediction;
+using UnityEngine.Serialization;
+
+public class Character2DControllerPrediction : NetworkBehaviour
+{
+    #region Types.
+        public struct MoveData
+        {
+            public float Horizontal;
+            public float Jump;
+        }
+        public struct ReconcileData
+        {
+            public Vector3 Position;
+            public Quaternion Rotation;
+            public ReconcileData(Vector3 position, Quaternion rotation)
+            {
+                Position = position;
+                Rotation = rotation;
+            }
+        }
+        #endregion
+
+        #region Serialized.
+        [SerializeField]
+        private float _moveRate = 5f;
+        [SerializeField]
+        private float _moveSpeed = 5f;
+        [SerializeField]
+        private float _jumpSpeed = 5f;
+        [SerializeField]
+        private float _jetPackSpeed = 5f;
+        #endregion
+
+        #region Private.
+        private Charecter2DController _characterController;
+        #endregion
+
+        private void Awake()
+        {
+            InstanceFinder.TimeManager.OnTick += TimeManager_OnTick;
+            _characterController = GetComponent<Charecter2DController>();
+        }
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();            
+            _characterController.enabled = (base.IsServer || base.IsOwner);
+        }
+
+        private void OnDestroy()
+        {
+            if (InstanceFinder.TimeManager != null)
+            {
+                InstanceFinder.TimeManager.OnTick -= TimeManager_OnTick;
+            }
+        }
+
+        private void TimeManager_OnTick()
+        {
+            if (base.IsOwner)
+            {
+                Reconciliation(default, false);
+                CheckInput(out Character2DControllerPrediction.MoveData md);
+                Move(md, false);
+            }
+            if (base.IsServer)
+            {
+                Move(default, true);
+                Character2DControllerPrediction.ReconcileData rd = new Character2DControllerPrediction.ReconcileData(transform.position, transform.rotation);
+                Reconciliation(rd, true);
+            }
+        }
+
+        private void CheckInput(out Character2DControllerPrediction.MoveData md)
+        {
+            md = default;
+
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float jump = Input.GetAxisRaw("Jump");
+
+            if (horizontal == 0f && jump == 0f)
+                return;
+
+            md = new Character2DControllerPrediction.MoveData()
+            {
+                Horizontal = horizontal,
+                Jump = jump
+            };
+        }
+
+        [Replicate]
+        private void Move(Character2DControllerPrediction.MoveData md, bool asServer, bool replaying = false)
+        {
+            Vector3 move = new Vector3(md.Horizontal* _moveSpeed, md.Jump*_jumpSpeed, 0) + new Vector3(0f, Physics.gravity.y, 0f);
+            //move = move.normalized;
+            Debug.Log("Jump:"+md.Jump*_jumpSpeed+"move:"+move);
+            _characterController.Move(move* _moveRate  * (float)base.TimeManager.TickDelta);
+        }
+
+        [Reconcile]
+        private void Reconciliation(Character2DControllerPrediction.ReconcileData rd, bool asServer)
+        {
+            transform.position = rd.Position;
+            transform.rotation = rd.Rotation;
+        }
+
+
+    }
---
+++ Assets/Scripts/Charecter2DController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FishNet.Object;
+using UnityEngine;
+using FishNet.Example.Prediction.CharacterControllers;
+
+public class Charecter2DController : NetworkBehaviour
+{
+    [SerializeField] private Transform _charecterTransform;
+    [SerializeField] private BoxCollider2D _character2DBoxCollider;
+    [SerializeField] private float _extraHeight = 0.1f;
+    private bool _isOnGround;
+
+    private void Update()
+    {
+        _isOnGround = IsGround();
+    }
+
+    public void Move(Vector3 magnitude )
+    {
+        if (_isOnGround)
+        {
+            if (magnitude.y < 0)
+            {
+                magnitude = new Vector3(magnitude.x, 0, magnitude.z);
+            }
+            _charecterTransform.position += magnitude;
+        }
+        else
+        {
+            _charecterTransform.position += magnitude;
+        }
+        
+    }
+
+    private bool IsGround()
+    {
+
+        Physics2D.Raycast(_character2DBoxCollider.bounds.center, Vector2.down,
+            _character2DBoxCollider.bounds.extents.y + _extraHeight);
+        Color rayColor;
+        Debug.DrawRay(_character2DBoxCollider.bounds.center, Vector2.down*
+            (_character2DBoxCollider.bounds.extents.y + _extraHeight));
+        return true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        /*Debug.Log("Touch something");
+        Debug.Log("CompareTag "+col.gameObject.tag + "obj name:"+col.gameObject.name);
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("Touch ground");
+            
+            _isOnGround = true;
+        }*/
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("Touch ground");
+            
+            //_isOnGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("leave ground");
+            //_isOnGround = false;
+        }
+    }
+}
